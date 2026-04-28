38749278e9513d965aa2cfe18ff58beb457a7886
jaelynnKhonrad <03081230048@student.uph.edu>
2026-04-28 12:31:52 +0700

    fixed

--- config/database.php
+++ config/database.php
@@ -59,9 +59,13 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => extension_loaded('pdo_mysql')
+            ? array_filter([
+                class_exists('Pdo\\Mysql')
+                    ? Pdo\Mysql::ATTR_SSL_CA
+                    : PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ])
+            : [],
         ],
 
         'pgsql' => [
